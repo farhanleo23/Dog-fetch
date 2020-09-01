047e8012a1058e10fab7e8e4059c9663e1741d5c
farhan hussain <farhanleo23@gmail.com>
2020-09-01 14:25:40 +0500

    display fetch api to console log

--- js/app.js
+++ js/app.js
@@ -2,11 +2,16 @@ const select = document.getElementById('breeds');
 const card = document.querySelector('.card'); 
 const form = document.querySelector('form');
 
+const randomDog = "https://dog.ceo/api/breeds/image/random";
+
+
 // ------------------------------------------
 //  FETCH FUNCTIONS
 // ------------------------------------------
 
-
+fetch(randomDog)
+    .then(response => response.json())
+    .then(data => console.log(data.message))
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
