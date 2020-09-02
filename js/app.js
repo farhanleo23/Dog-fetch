21243391f87d7b44a58e7e96f7f60b69cf9f2196
farhan hussain <farhanleo23@gmail.com>
2020-09-02 17:29:42 +0500

    add function for option breeds

--- js/app.js
+++ js/app.js
@@ -3,13 +3,23 @@ const card = document.querySelector('.card');
 const form = document.querySelector('form');
 
 const randomDog = "https://dog.ceo/api/breeds/image/random";
+const breedList = "https://dog.ceo/api/breeds/list";
 
 
+fetch(breedList)
+.then(response => response.json())
+.then(data => generateOptions(data.message))
 
 fetch(randomDog)
     .then(response => response.json())
     .then(data => generateImage(data.message) )
 
+
+function generateOptions(data){
+    const options = data.map(item => `
+    <option value="${item}">${item}</option>`).join('');
+    select.innerHTML = options;
+}
 
 function generateImage(data){
     const html = `
